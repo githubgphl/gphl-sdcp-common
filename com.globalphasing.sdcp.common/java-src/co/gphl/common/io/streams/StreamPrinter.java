7299ad005cb215e6202f70d36aaeb486e820fd33
Peter Keller <pkeller@globalphasing.com>
2016-05-23 15:07:01 +0100

    Implement low-latency buffering for transmitting stream output
     - ActiveMQ struggles to keep up with one line per message. This commit
    uses a buffer to increase the message size when the stream's output rate
    is high, but allows a configurable maximum latency when its output rate
    is low.

--- com.globalphasing.sdcp.common/java-src/co/gphl/common/io/streams/StreamPrinter.java
+++ com.globalphasing.sdcp.common/java-src/co/gphl/common/io/streams/StreamPrinter.java
@@ -21,6 +21,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.Writer;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 
 /**
  * Class to use a {@link PrintStream} to output the data being received
@@ -35,9 +39,17 @@ import java.io.Writer;
  *  int status = process.waitFor();
  * </pre>
  *
- * Adapted from a suggestion by Boris Daich at
- * <a href="http://stackoverflow.com/a/1732506/1866402">
- * http://stackoverflow.com/questions/1732455/redirect-process-output-to-stdout</a>.
+ * <p>The code to handle the stream data line-by-line has been adapted from a
+ * suggestion by Boris Daich at <a href="http://stackoverflow.com/a/1732506/1866402">
+ * http://stackoverflow.com/questions/1732455/redirect-process-output-to-stdout</a>.</p>
+ * 
+ * <p>The code to decode the byte buffer has been adapted from a FileChannel example
+ * in "Java Examples in a Nutshell", 3rd Edition (Flanagan, 2004) found at
+ * <a href="http://www.java2s.com/Code/Java/File-Input-Output/ReadbytesfromthespecifiedchanneldecodethemusingthespecifiedCharsetandwritetheresultingcharacterstothespecifiedwriter.htm">
+ * http://www.java2s.com/Code/Java/File-Input-Output/ReadbytesfromthespecifiedchanneldecodethemusingthespecifiedCharsetandwritetheresultingcharacterstothespecifiedwriter.htm</a>.
+ * </p>
+ * 
+ * 
  *
  * @see java.lang.ProcessBuilder
  * 
@@ -50,8 +62,30 @@ public class StreamPrinter extends Thread {
     private File outputFile;
     private boolean append;
     private String header;
+    final private int cbufSize, delay;
+    
+    // FIXME! Specify charset for both line-by-line and buffered handling
+    // via a system property.
+    private static final Charset charset = Charset.defaultCharset();
 
-    String lastLine = null;
+    private String lastLine = null;
+    private CharBuffer lineSepBuf;
+    private final boolean captureLastLine;
+    
+    /**
+     * System property name for sizing internal character buffer, where caller has specified
+     * the default size. The value may be negative, to switch to line-by-line operation
+     */
+    public final static String BUFSIZEPROPERTY = "co.gphl.streamprinter.cbufsize";
+    public final static int defaultBufSize = 4096;
+    
+    /**
+     * System property name for stream read delay, where caller has specified
+     * the default. The value may be negative, to disable the read delay. This will lower
+     * the latency, but may reduce the efficiency of the buffering.
+     */
+    public final static String DELAYPROPERTY = "co.gphl.streamprinter.delay";
+    public final static int defaultDelay = 100;
     
     /**
      * Capture the line-by-line output of a stream and direct it to a {@link Writer}, a {@link File}, or both.
@@ -69,13 +103,106 @@ public class StreamPrinter extends Thread {
      */
     public StreamPrinter(InputStream inputStream, Writer outputWriter, File outputFile,
                          boolean append, String header) {
+        this(inputStream, outputWriter, outputFile, append, header, -1, -1, true);
+    }
+
+    /**
+     * Capture the output of a stream and direct it to a {@link Writer}, a {@link File}, or both.
+     * When instantiated via this constructor, the StreamPrinter attempts to provide the
+     * best of both worlds:
+     * 
+     * <ul>
+     * <li>when the rate of output is high, efficient transfer using a buffer</li>
+     * <li>when the rate of output is low, latency at the destination(s) almost as
+     * low as if using blocking, unbuffered I/O</li>
+     * </ul>
+     * 
+     * The stream data are read into a buffer, whose contents are sent to the
+     * destination(s) when either:
+     * 
+     * <ul>
+     * <li>the buffer fills up, or</li>
+     * <li>no data are available from the stream for a period of {@code delay} milliseconds</li>
+     * </ul>
+     * 
+     * <p>{@code outputFile} is not flushed to reduce the likelihood of writes to it blocking.
+     * If output to only one of {@code outputWriter} and {@code outputFile} is needed,
+     * use {@code null} for the other one.</p>
+     * 
+     * <p>The last line of the output will not be available on platforms
+     * where the line separator string is more than one character long, when using buffering. An
+     * {@code UnsupportedOperationException} will be thrown if the arguments to this constructor
+     * imply a violation of this condition.</p>
+     * 
+     * @param inputStream
+     * @param outputWriter
+     * @param outputFile
+     * @param append if {@code true}, output is appended to {@code outputFile}.
+     * @param header if not {@code null}, start output with this string
+     * @param cbufSize size of character buffer to be used for reading from inputStream.
+     * {@code if (cbufSize < 0)} line-by-line behaviour will be used. {@code if (cbufSize == 0)}
+     * the buffer size will be set from the system property {@code co.gphl.streamprinter.cbufsize} if set,
+     * otherwise an internal default of 4096 characters will be used.
+     * @param delay number of milliseconds to wait for more input, before sending
+     * the output received so far to the destination(s). {@code if (delay < 0)}, the delay
+     * will be disabled. {@code if (delay == 0)}, the delay will be set from the system property
+     * {@code co.gphl.streamprinter.delay} if set, otherwise an internal default of 100
+     * milliseconds will be used.
+     * @param captureLastLine if {@code true} the last line of the stream will be available
+     * from the method {@link #getLastLine()}
+     * 
+     * @throws UnsupportedOperationException if
+     * {@code captureLastLine && cbufSize >= 0 && System.getProperty("line.separator").length() > 1}
+     */
+    public StreamPrinter(InputStream inputStream, Writer outputWriter, File outputFile,
+            boolean append, String header, int cbufSize, int delay, boolean captureLastLine) {
+
         this.is = inputStream;
         this.outputWriter = outputWriter;
         this.outputFile = outputFile;
         this.append = append;
         this.header = header;
-    }
+        this.captureLastLine = captureLastLine;
 
+        if ( cbufSize == 0 ) {
+            String cbufSizeStr = System.getProperty(StreamPrinter.BUFSIZEPROPERTY);
+            if ( cbufSizeStr != null )
+                cbufSize = Integer.parseInt(cbufSizeStr);
+        }
+        
+        if ( cbufSize == 0 )
+            cbufSize = StreamPrinter.defaultBufSize;
+        
+        this.cbufSize = cbufSize;
+
+        if ( delay == 0 ) {
+            String delayStr = System.getProperty(StreamPrinter.DELAYPROPERTY);
+            if ( delayStr != null )
+                delay = Integer.parseInt(delayStr);
+        }
+        
+        if ( delay == 0 )
+            delay = StreamPrinter.defaultDelay;
+        else if ( delay < 0 )
+            // Disabling the delay is done by setting the true internal delay to 0ms, so
+            // we translate an input value of < 0 to 0 here.
+            delay = 0;
+        
+        this.delay = delay;
+        
+        if ( this.cbufSize > 0 ) {
+            this.lineSepBuf = CharBuffer.wrap(System.getProperty("line.separator"));
+            
+            // The algorithm in the buffered fetch will fail to identify the last line correctly
+            // if the line separator string is more than one character long and it happens to
+            // be split across two reads from the input stream.
+            if ( this.lineSepBuf.length() > 1 && this.captureLastLine )
+                throw new UnsupportedOperationException("Use line-by-line operation on platforms "
+                        + "where the line separator string is more then one character");
+        }
+        
+    }
+    
     @Override
     public void run() {
 
@@ -87,33 +214,12 @@ public class StreamPrinter extends Thread {
             if ( this.outputWriter != null )
                 this.outputWriter.write(this.header);
 
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
 
-            String line=null;
-
-            // For efficiency, don't test for destinations inside a
-            // "while ( (line = br.readLine()) != null)" loop
-            if ( this.outputWriter != null && fileWriter == null ) {
-                while ( (line = br.readLine()) != null) {
-                    this.outputWriter.write(line + "\n");
-                    this.outputWriter.flush();
-                    this.lastLine = line;
-                }
-            }
-            else if ( this.outputWriter != null && fileWriter != null ) {
-                while ( (line = br.readLine()) != null) {
-                    this.outputWriter.write(line + "\n");
-                    this.outputWriter.flush();
-                    fileWriter.write(line + "\n");
-                    // Don't flush file writer.
-                    this.lastLine = line;
-                }
-            }
-            else if ( this.outputWriter == null && fileWriter != null ) {
-                fileWriter.write(line + "\n");
-                this.lastLine = line;
-            }
+            if ( this.cbufSize <= 0 )
+                this.pipeLineByLine(new BufferedReader(new InputStreamReader(is)), fileWriter);
+            else
+                this.pipeWithBuffer(is, fileWriter);
+            
         }
         catch (IOException e) {
             // Don't abort workflow over this: it may still run OK even if
@@ -122,8 +228,207 @@ public class StreamPrinter extends Thread {
         }
     }
 
+    private void pipeLineByLine(BufferedReader br, Writer fileWriter) throws IOException {
+
+        String line=null;
+
+        // For efficiency, don't test for destinations inside a
+        // "while ( (line = br.readLine()) != null)" loop
+        if ( this.outputWriter != null && fileWriter == null ) {
+            while ( (line = br.readLine()) != null) {
+                this.outputWriter.write(line + "\n");
+                this.outputWriter.flush();
+                this.lastLine = line;
+            }
+        }
+        else if ( this.outputWriter != null && fileWriter != null ) {
+            while ( (line = br.readLine()) != null) {
+                this.outputWriter.write(line + "\n");
+                this.outputWriter.flush();
+                fileWriter.write(line + "\n");
+                // Don't flush file writer.
+                this.lastLine = line;
+            }
+        }
+        else if ( this.outputWriter == null && fileWriter != null ) {
+            fileWriter.write(line + "\n");
+            this.lastLine = line;
+        }
+
+    }
+    
+    /* Reading from the stream directly rather than via a Reader is the key to
+     * doing non-blocking I/O: we can use the available() method to ensure that
+     * reads don't block when we don't want them to. The downsides are:
+     * 
+     * (1) we have to handle the conversion from bytes to characters ourselves, although
+     * that isn't hard, 
+     * 
+     * (2) capturing the last line of output accurately (needed to check the exit status
+     * of stratcal and simcal_predict) is fiddly
+     * 
+     * Unfortunately, Process/ProcessBuilder don't provide NIO access to stdout/stderr, so
+     * we have to resort to some low-level trickery.
+     */
+    private void pipeWithBuffer(InputStream is, Writer fileWriter) throws IOException {
+
+        CharBuffer cbuf = CharBuffer.allocate(this.cbufSize);
+        ByteBuffer bbuf = ByteBuffer.allocate(this.cbufSize);
+        byte[] barray = bbuf.array();
+        char[] carray = cbuf.array();
+        int readThisTime = 0, available, bpos, cpos, clim;
+
+        // These variables are used in the code that captures the last line of stdout
+        StringBuilder lastLineBldr = new StringBuilder(200);
+        int lineSepLen = lineSepBuf.length(), startOfLastLine, searchBolFrom;
+        boolean gotEol = false;
+        
+        CharsetDecoder decoder = StreamPrinter.charset.newDecoder();
+        
+        do {
+
+            /* This inner loop does the following:
+             * (i) If the receiving buffer is empty, read (blocking) until we get something from
+             * the input stream.
+             * (ii) If the receiving buffer is not empty and there is something available
+             * from the stream, read it (non-blocking) and go around the loop again.
+             * (iii) If the receiving buffer is not empty and there is nothing available from
+             * the input stream, pause for this.delay (in milliseconds) and check again. If something has
+             * become available, handle as (ii)
+             * 
+             * We exit the loop on one of the following conditions:
+             * (1) Receiving buffer is full (loop's while(...) condition not satisfied)
+             * (2) A read from the stream indicates end-of-stream (break)
+             * (3) In case (iii), nothing more is available from the stream after
+             * the second check (break)
+             * 
+             * On exit from this loop, if the buffer is not empty its contents are decoded
+             * and emitted.
+             */
+            do {
+                available = is.available();
+                bpos = bbuf.position();
+
+                if ( available == 0 && bpos > 0 ) {
+
+                    try {
+                        Thread.sleep(this.delay);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    available = is.available();
+                    if ( available == 0 )
+                        break;
+                }
+                
+                if ( available == 0 )
+                    // bpos can only be 0 if available == 0 here, so OK to use the
+                    // single-argument form of read()
+                    readThisTime = is.read( barray );
+                else
+                    // Never let the len parameter of is.read(...) be 0, otherwise we will not
+                    // detect the end of stream and never exit this loop.
+                    readThisTime = is.read( barray, bpos, Math.min( available, bbuf.remaining() ) );
+
+                if ( readThisTime < 0 )
+                    break;
+                bbuf.position( bpos + readThisTime );
+
+            } while ( bbuf.hasRemaining() );
+
+            /* When the input stream is producing data rapidly, we will be handling
+             * it in large chunks.
+             * Testing for this.outputWriter/this.fileWriter != null each time we emit
+             * the buffer contents is not an efficiency concern here, unlike
+             * in the line-by-line case.
+             */ 
+            if ( bbuf.position() > 0 ) {
+                
+                bbuf.flip();
+                decoder.decode(bbuf, cbuf, readThisTime < 0);
+                cbuf.flip();
+                
+                cpos = cbuf.position();
+                clim = cbuf.limit();
+                
+                if ( this.outputWriter != null ) {
+                    this.outputWriter.write(cbuf.array(), cpos, cbuf.remaining());
+                    this.outputWriter.flush();
+                }
+                if ( fileWriter != null )
+                    fileWriter.write(cbuf.array(), cpos, cbuf.remaining());
+                
+                // Fiddly code to get last line from buffer, allowing for the fact
+                // that the last line may be split across more than one read from
+                // the stream. We avoid the use of java.lang.String as far as possible 
+                // to reduce the amount of copying of characters from one place to another
+                // (most methods/expressions that return an instance of String involve
+                // copying character data around).
+                
+                if ( this.captureLastLine && clim >= lineSepLen ) {
+                
+                    // Where in the buffer do we step backwards from, to look for the start of the last line?
+                    // The end of the buffer, unless the buffer ends with a newline, in which case
+                    // we start from immediately before the newline.
+                    searchBolFrom = clim;
+                    if ( cbuf.subSequence(clim - lineSepLen, clim).compareTo(lineSepBuf) == 0 ) {
+                        gotEol = true;
+                        searchBolFrom -= lineSepLen;
+                    }
+                
+                    // Now step backwards to find the start of the last line. If we find it,
+                    // we discard anything that we have accumulated from earlier iterations
+                    // as (part of) the putative last line.
+                    for ( startOfLastLine = searchBolFrom - 1; startOfLastLine >= cpos; startOfLastLine-- ) {
+                        if ( cbuf.subSequence(startOfLastLine, startOfLastLine + lineSepLen).compareTo(lineSepBuf) == 0) {
+                            lastLineBldr.delete(0, lastLineBldr.length());
+                            startOfLastLine += lineSepLen;
+                            break;
+                        }
+                    }
+                    
+                    // If we emerged from the end of the for loop, we will have gone one character too far
+                    // back.
+                    if ( startOfLastLine < cpos )
+                        startOfLastLine = cpos;
+                    
+                    lastLineBldr.append(carray, startOfLastLine, searchBolFrom - startOfLastLine);
+                    
+                    if ( gotEol ) {
+                        this.lastLine = lastLineBldr.toString();
+                        // Reset state, in case this isn't in fact the
+                        // last line and more output is on its way.
+                        lastLineBldr.delete(0, lastLineBldr.length());
+                        gotEol = false;
+                    }
+                    else
+                        this.lastLine = null;
+                    
+                }
+                bbuf.compact();
+                cbuf.clear();
+            }
+            
+        } while ( readThisTime >= 0 );
+    }
+    
+    public boolean isCaptureLastLine() {
+        return this.captureLastLine;
+    }
+    
+    /**
+     * Returns the last line of output from the input stream.
+     * 
+     * @return last line of output.
+     * 
+     * @throws UnsupportedOperationException {@code if ( !this.isCaptureLastLine() )}
+     */
     public String getLastLine() {
-        return lastLine;
+        if ( ! this.captureLastLine )
+            throw new UnsupportedOperationException("This StreamPrinter has not been configured to "
+                    + "capture the last line of output");
+        return this.lastLine;
     }
     
 }
